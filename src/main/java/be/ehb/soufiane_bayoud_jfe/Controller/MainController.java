d161131c576c28596dfd45bad978af470f88de80
SoufianeBayoud <soufiane.bayoud@student.ehb.be>
2022-06-13 09:32:28 +0200

    Verschillende DAO aangemaakt

---
+++ src/main/java/be/ehb/soufiane_bayoud_jfe/Controller/MainController.java
@@ -0,0 +1,4 @@
+package be.ehb.soufiane_bayoud_jfe.Controller;
+
+public class MainController {
+}
---
+++ src/main/java/be/ehb/soufiane_bayoud_jfe/Model/HuurderDAO.java
@@ -0,0 +1,6 @@
+package be.ehb.soufiane_bayoud_jfe.Model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HuurderDAO extends CrudRepository<Huurder, Integer> {
+}
