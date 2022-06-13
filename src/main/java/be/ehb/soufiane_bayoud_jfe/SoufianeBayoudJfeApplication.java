f79d1bade71f1e7e1811229c726a0284b69955bb
SoufianeBayoud <soufiane.bayoud@student.ehb.be>
2022-06-13 09:17:46 +0200

    Initial commit

---
+++ src/main/java/be/ehb/soufiane_bayoud_jfe/SoufianeBayoudJfeApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.soufiane_bayoud_jfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoufianeBayoudJfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoufianeBayoudJfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ehb/soufiane_bayoud_jfe/SoufianeBayoudJfeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.soufiane_bayoud_jfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoufianeBayoudJfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
