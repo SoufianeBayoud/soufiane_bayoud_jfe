67d5c0e29d477bdcf585e1e7ddd717a0ddc8a19e
SoufianeBayoud <soufiane.bayoud@student.ehb.be>
2022-06-13 09:39:35 +0200

    Opvragen van alle auto’s gemaakt

--- src/main/java/be/ehb/soufiane_bayoud_jfe/Controller/MainController.java
+++ src/main/java/be/ehb/soufiane_bayoud_jfe/Controller/MainController.java
@@ -1,4 +1,32 @@
 package be.ehb.soufiane_bayoud_jfe.Controller;
 
+import be.ehb.soufiane_bayoud_jfe.Model.Auto;
+import be.ehb.soufiane_bayoud_jfe.Model.AutoDAO;
+import be.ehb.soufiane_bayoud_jfe.Model.HuurderDAO;
+import be.ehb.soufiane_bayoud_jfe.Model.VerhuurDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    private AutoDAO autoDAO;
+    private HuurderDAO huurderDAO;
+    private VerhuurDAO verhuurDAO;
+
+    @Autowired
+    public MainController(AutoDAO autoDAO, HuurderDAO huurderDAO, VerhuurDAO verhuurDAO) {
+        this.autoDAO = autoDAO;
+        this.huurderDAO = huurderDAO;
+        this.verhuurDAO = verhuurDAO;
+    }
+
+    @RequestMapping(value = "api/cars", method = RequestMethod.GET)
+    public Iterable<Auto> findAll() {
+        return autoDAO.findAll();
+    }
+
+
 }
